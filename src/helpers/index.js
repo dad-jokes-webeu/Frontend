7bd8d5e76cdb10735f4ef7944638e695f447bd9c
rodrigograca31 <rodrigograca1993@gmail.com>
2019-11-18 12:56:54 +0000

    ‍🚀 Environment helper

---
+++ src/helpers/index.js
@@ -0,0 +1,10 @@
+const herokuBaseURL = "https://dadjokes-api.herokuapp.com/api/";
+const localhostBaseURL = "http://localhost:5000/api/";
+
+// this allows react to figure out
+// whether to use local or heroku URL for the calls to the API
+export function withBaseURL(endpoint) {
+	return process.env.NODE_ENV === "production"
+		? herokuBaseURL + endpoint
+		: localhostBaseURL + endpoint;
+}
