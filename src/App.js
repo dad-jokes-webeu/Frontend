166f9bf57e27d5546640f76ff3a543d2d0290de6
Darragh Ferry <darraghferry@hotmail.com>
2019-11-18 16:33:15 +0000

    deleted some unnecessary lines of code

--- src/App.js
+++ src/App.js
@@ -5,30 +5,11 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import { Route } from "react-router-dom";
 import List from "./components/JokesList";
-// import * as help from "./helpers";
-// import axios from "axios";
 
 function App() {
-	// const [response, setResponse] = useState(null);
-
-	// const getRequest = api => {
-	// 	axios.get(help.withBaseURL(api)).then(res => {
-	// 		console.log(res.data);
-
-	// 		setResponse(res.data);
-	// 	});
-	// };
-
 	return (
 		<div className="App">
 			<Navbar />
-			{/* <header
-				className="App-header"
-				// onClick={e => getRequest("jokes/public")}
-			>
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>This the next big app for Dad Jokes</p>
-			</header> */}
 			<Route
 				exact
 				path="/"
