27c2b18a03de18a59d809c6cdbd316f4f533820e
rodrigograca31 <rodrigograca1993@gmail.com>
2019-11-19 16:48:13 +0000

    💄🎨 Login and Register made pretty

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,30 +2,97 @@ import React, { useState } from "react";
 import { withFormik, Form, Field, ErrorMessage } from "formik";
 import * as yup from "yup";
 import axiosWithAuth from "../helpers/axios";
+import { Alert, Input, FormGroup, Label, FormFeedback } from "reactstrap";
 
 function UserForm(props) {
 	return (
-		<div>
-			<Form>
-				<label>
-					Username:
-					<Field type="text" name="username" placeholder="Username" />
-				</label>
-				<label>
-					Email:
-					<Field type="text" name="email" placeholder="Email" />
-				</label>
-				<label>
-					Password
-					<Field
-						type="password"
-						name="password"
-						placeholder="Password"
-					/>
-				</label>
-				<input type="submit" />
-			</Form>
-		</div>
+		<Form>
+			<FormGroup>
+				<Label>Username:</Label>
+				<Field
+					name="username"
+					render={({ field, form: { touched, errors } }) => (
+						<>
+							<Input
+								{...field}
+								type="text"
+								invalid={
+									!!(
+										touched[field.name] &&
+										errors[field.name]
+									)
+								}
+								placeholder="Email please?"
+							/>
+							{touched[field.name] && errors[field.name] && (
+								<FormFeedback color="danger">
+									{errors[field.name]}
+								</FormFeedback>
+							)}
+						</>
+					)}
+				/>
+			</FormGroup>
+			<FormGroup>
+				<Label>Email:</Label>
+				<Field
+					name="email"
+					render={({ field, form: { touched, errors } }) => (
+						<>
+							<Input
+								{...field}
+								type="email"
+								invalid={
+									!!(
+										touched[field.name] &&
+										errors[field.name]
+									)
+								}
+								placeholder="Email please?"
+							/>
+							{touched[field.name] && errors[field.name] && (
+								<FormFeedback color="danger">
+									{errors[field.name]}
+								</FormFeedback>
+							)}
+						</>
+					)}
+				/>
+			</FormGroup>
+			<FormGroup>
+				<Label>Password:</Label>
+				<Field
+					name="password"
+					render={({ field, form: { touched, errors } }) => (
+						<>
+							<Input
+								{...field}
+								type="password"
+								invalid={
+									!!(
+										touched[field.name] &&
+										errors[field.name]
+									)
+								}
+								placeholder="Password please?"
+							/>
+							{touched[field.name] && errors[field.name] && (
+								<FormFeedback color="danger">
+									{errors[field.name]}
+								</FormFeedback>
+							)}
+						</>
+					)}
+				/>
+			</FormGroup>
+			<Input type="submit" />
+			<br />
+			{props.status && props.status.error && (
+				<Alert color="danger">
+					There was an error, please try again
+				</Alert>
+			)}
+		</Form>
 	);
 }
 
@@ -39,13 +106,21 @@ const RegisterForm = withFormik({
 	},
 
 	validationSchema: yup.object().shape({
-		username: yup.string().required("username required"),
-		email: yup.string().required("password needed"),
-		password: yup.string().required("Please enter a password")
+		username: yup
+			.string()
+			.min(3, "Minimum 3 characters")
+			.required("Username is required"),
+		email: yup
+			.string()
+			.email("Please enter a valid email")
+			.required("Email is required"),
+		password: yup
+			.string()
+			.min(3, "Minimum 3 characters")
+			.required("Password is required")
 	}),
 
-	handleSubmit(values) {
-		console.log(values);
+	handleSubmit(values, { props, setStatus }) {
 		axiosWithAuth()
 			.post("auth/register", {
 				username: values.username,
@@ -53,9 +128,12 @@ const RegisterForm = withFormik({
 				password: values.password
 			})
 			.then(response => {
+				props.history.push("/login");
 				console.log(response);
 			})
 			.catch(error => {
+				console.log(props);
+				setStatus({ error: true });
 				console.log(error);
 			});
 	}
