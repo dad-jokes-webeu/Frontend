a8cc80be52ae9246ba16edb4a41a20e4752b7893
Darragh Ferry <darraghferry@hotmail.com>
2019-11-19 10:37:17 +0000

    linked up register button with register route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ export default function Navbar() {
 		<NavContainer>
 			<img src={logo} alt="logo" />
 			<Nav>
-				<Link to='#'>
+				<Link to="#">
 					<Button outline color="primary">
 						Knock-knock
 					</Button>{" "}
@@ -49,10 +49,10 @@ export default function Navbar() {
 						Who's there?
 					</DropdownToggle>
 					<DropdownMenu>
-						<Link to='#'>
+						<Link to="#">
 							<DropdownItem>Sign In</DropdownItem>
 						</Link>
-						<Link to='#'>
+						<Link to="/register">
 							<DropdownItem>Register</DropdownItem>
 						</Link>
 					</DropdownMenu>
